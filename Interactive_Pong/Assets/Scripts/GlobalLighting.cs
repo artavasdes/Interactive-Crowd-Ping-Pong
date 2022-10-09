75efef879d717efa3e0c9ebf70d3b07dd0d57d5d
artavasdes <vartanyildiz@gmail.com>
2022-10-08 22:17:42 -0400

    Completed all gamemodes

--- Interactive_Pong/Assets/Scripts/GlobalLighting.cs
+++ Interactive_Pong/Assets/Scripts/GlobalLighting.cs
@@ -8,23 +8,21 @@ public class GlobalLighting : MonoBehaviour
     UnityEngine.Rendering.Universal.Light2D GlobalLight;
     void Start()
     {
-        StartCoroutine(ExampleCoroutine());
-
+        if (GameModeSetter.GameMode == "Flashlight"){
+            StartCoroutine(ExampleCoroutine());
+        }
+        
     }
-
 
     IEnumerator ExampleCoroutine()
     {
-        //Print the time of when the function is first called.
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
 
-        //yield on a new YieldInstruction that waits for 5 seconds.
         yield return new WaitForSeconds(1);
 
         GlobalLight = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
         GlobalLight.intensity = 0f;
 
-        //After we have waited 5 seconds print the time again.
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 }
--- Interactive_Pong/Assets/Scripts/Menu.cs
+++ Interactive_Pong/Assets/Scripts/Menu.cs
@@ -3,8 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void Play()
+    public void PlayNormal()
     {
-        SceneManager.LoadScene("FlashlightPong");
+        SceneManager.LoadScene("Pong");
+        GameModeSetter.GameMode = "Normal";
+    }
+
+    public void PlayHard()
+    {
+        SceneManager.LoadScene("Pong");
+        GameModeSetter.GameMode = "Hard";
+    }
+
+    public void PlayFlashlight()
+    {
+        SceneManager.LoadScene("Pong");
+        GameModeSetter.GameMode = "Flashlight";
     }
 }
